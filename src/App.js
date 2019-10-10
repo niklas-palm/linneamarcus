829e166d8fb7447bb27d7a54ac912899d94b438d
Palm <nipalm@f45c89acd433.ant.amazon.com>
2019-10-10 17:03:24 +0200

    Install material-ui and add api controler

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+import "./App.scss";
+
+import api from "./controler";
 
 function App() {
+  const [controler] = useState(new api());
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Vi gifter oss och ni är inte bjudna!
-        </p>
-      </header>
-    </div>
+    <CssBaseline>
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Vi gifter oss och ni är inte bjudna!</p>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => controler.post()}
+          >
+            Press me!
+          </Button>
+        </div>
+      </div>
+    </CssBaseline>
   );
 }
 
