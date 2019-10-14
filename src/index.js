70f17533e8de98cf4b0813f48593182e0c75a6d6
Palm <nipalm@f45c89acd433.ant.amazon.com>
2019-10-14 17:56:10 +0200

    remove unused dep

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import "./index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-import { BrowserRouter as Router } from "react-router-dom";
-
 ReactDOM.render(
   <CssBaseline>
     <App />
