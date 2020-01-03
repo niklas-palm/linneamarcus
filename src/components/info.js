7a131dfaef6f84b3953ea130cb4054723f042588
Palm <nipalm@f45c89acd433.ant.amazon.com>
2020-01-03 15:09:42 +0100

    Add info content

--- src/components/info.js
+++ src/components/info.js
@@ -1,13 +1,66 @@
 import React from "react";
 
-import "../styles/info.scss";
-
 const Info = () => {
   return (
-    <div className="InfoContainer">
-      <h1>
-        <span className="FirstLetter">I</span> <span>nfo</span>
-      </h1>
+    <div className="LandingContainer">
+      <div className="left">
+        <h1>
+          <span className="FirstLetter">B</span> <span>oende</span>
+        </h1>
+        <p className="intro">
+          Vi har abonnerat ett antal hotellrum för våra gäster på Scandic S:t
+          Jörgen som är beläget i närheten av centralstationen i Malmö. Rummen
+          finns tillgängliga för bokning till och med den 31 maj 2020, därefter
+          släpps vår reservation så se till att boka innan dess!
+        </p>
+        <p>
+          <b>OBS:</b> Vid bokningen uppger ni koden [xx] för att erhålla ett
+          rabatterat pris.
+        </p>
+        <p>
+          I Malmö finns det ju förstås även en stor variation av andra
+          boendemöjligheter, t.ex. via Airbnb, hotell eller vandrarhem.
+        </p>
+      </div>
+
+      <div className="divider"></div>
+
+      <div className="right">
+        <h1>
+          <span className="FirstLetter">A</span> <span>tt ta sig dit</span>
+        </h1>
+        <p className="intro">
+          Tänk på att boka resa i god tid för att slippa onödiga kostnader när
+          priserna stiger. Scandic S:t Jörgen i Malmö ligger centralt beläget på
+          cirka 10 minuters gångavstånd från Malmös centralstation. Ni tar er
+          enkelt till Malmö via:
+        </p>
+        <p style={{ color: "red" }}>
+          Vill ni lista olika alternativ här? Är det ingenting folk kan lista ut
+          på egen hand?
+        </p>
+      </div>
+
+      <div className="left">
+        <h1>
+          <span className="FirstLetter">T</span> <span>al</span>
+        </h1>
+        <p className="intro">
+          Vill du hålla tal eller göra något annat spex under middagen, kontakta
+          våra eminenta toastmadames Jenny Palmgren och Hanna Arvidsson på xxx
+          senast den 10 juli 2020.
+        </p>
+      </div>
+
+      <div className="right">
+        <h1>
+          <span className="FirstLetter">P</span> <span>resenter</span>
+        </h1>
+        <p className="intro">
+          Vi önskar oss inte några presenter, endast att ni kommer och firar med
+          oss!
+        </p>
+      </div>
     </div>
   );
 };
