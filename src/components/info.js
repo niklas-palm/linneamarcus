33e3cbb34c62f2e6a14c4b31ab5a8e489e1ce20a
Palm <nipalm@f45c89acd433.ant.amazon.com>
2020-01-05 17:54:05 +0100

    Change header font + add hotel info

--- src/components/info.js
+++ src/components/info.js
@@ -8,14 +8,48 @@ const Info = () => {
           <span className="FirstLetter">B</span> <span>oende</span>
         </h1>
         <p className="intro">
-          Vi har abonnerat ett antal hotellrum för våra gäster på Scandic S:t
+          Vi har reserverat ett antal hotellrum för våra gäster på Scandic S:t
           Jörgen som är beläget i närheten av centralstationen i Malmö. Rummen
           finns tillgängliga för bokning till och med den 31 maj 2020, därefter
           släpps vår reservation så se till att boka innan dess!
         </p>
         <p>
-          <b>OBS:</b> Vid bokningen uppger ni koden [xx] för att erhålla ett
-          rabatterat pris.
+          Det finns även ett par flerbäddsrum och singelrum reserverade,
+          vänligen kontakta hotellet på ovan telefonnummer om ni önskar boka ett
+          sådant rum. Glöm inte att uppge rabattkoden!
+        </p>
+        <p style={{ marginBottom: "0" }}>
+          Bokning av hotellrum kan göras via:{" "}
+        </p>
+        <ul>
+          <li>
+            <a
+              style={{ color: "inherit" }}
+              href="www.scandichotels.se/stjorgen?bookingcode=BSTA310720"
+              target="_blank"
+            >
+              <p> Scandics hemsida</p>
+            </a>
+          </li>
+          <li>
+            <p>
+              Telefon:{" "}
+              <a
+                style={{
+                  color: "inherit",
+                  textDecoration: "none"
+                }}
+                href="tel:+46 40 693 46 00"
+              >
+                +46 40 693 46 00
+              </a>
+              , tonval 1
+            </p>
+          </li>
+        </ul>
+        <p>
+          <b>OBS:</b> Vid bokningen uppger ni koden BSTA310720 för att erhålla
+          ett rabatterat pris (750 kr för ett dubbelrum per natt).
         </p>
         <p>
           I Malmö finns det ju förstås även en stor variation av andra
